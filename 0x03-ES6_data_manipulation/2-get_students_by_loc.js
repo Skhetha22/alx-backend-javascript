884186ce40f607afe7c5f94f07ac9b802e9c6c23
root <rabsonncubez@gmail.com>
2024-07-01 16:49:51 +0000

    0x03-ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Rabson Maratshi <https://github.com/Skhetha22>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Rabson Maratshi <https://github.com/Skhetha22>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
